63c8d35f59a3dad06d1f8ceef29b6ae8f32aadd5
Brahim006 <edu_ali_14@hotmail.com>
2020-07-17 18:57:48 -0300

    Inicializado npm, implementado framework express y programadas las primeras peticiones al servidor

--- site/app.js
+++ site/app.js
@@ -1,1 +1,18 @@
-// TODO: Escribir el punto de entrada al servidor en este archivo
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static("public")); // Declaración de carpeta estática
+
+app.listen(PORT, () => console.log("Servidor funcionando en el puerto " + PORT));
+
+/* 
+    Región de preticiones tipo GET
+*/
+
+app.get("/", (req, res) => {
+
+    res.sendFile(__dirname + "/views/index.html");
+
+});
