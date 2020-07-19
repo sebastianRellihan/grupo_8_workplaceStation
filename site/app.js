097e4761e970808546c188255ed35dd658c73fdb
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-07-19 15:38:48 -0300

    Añadidas respuestas del servidor a peticiones tipo GET para las urls /register y /login

--- site/app.js
+++ site/app.js
@@ -14,3 +14,11 @@ app.listen(PORT, () => console.log("Servidor funcionando en el puerto " + PORT))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+});
