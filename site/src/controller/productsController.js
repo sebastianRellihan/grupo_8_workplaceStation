ddfce3f6d7f3f3c4f7c19754cb2b94240305b052
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-08-05 16:54:58 -0300

    Parseado el JSON de información de productos y enviados a la vista de 'products' en forma de objeto literal

--- site/src/controller/productsController.js
+++ site/src/controller/productsController.js
@@ -3,11 +3,16 @@
  * Como por ejemplo: /products, /products/detail, products/cart, etc...
  * Debe ser llamado desde el enrutador productsRoutes.
 */
+const fs = require("fs");
+const path = require("path");
+
+let dataJSON = fs.readFileSync(path.join(__dirname, "..", "data", "products.json"));
+let dataObject = JSON.parse(dataJSON);
 
 module.exports = {
     /** Envía la vista principal de todos los productos (products.ejs) */
     index: (req, res) => {
-        res.render("products/products");
+        res.render("products/products", {products: dataObject});
     },
     /** Envía la vista del detalle de producto (product-detail.ejs) */
     detail: (req, res) => {
