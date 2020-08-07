2f81cfb72265c01e520b9e5879f5ca09455775bf
sebastianRellihan <srellihan@gmail.com>
2020-08-07 04:38:06 -0300

    Ruta post del form de creacion de producto

--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -7,14 +7,22 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/productsController");
 
+// Todos los productos
 router.get("/", controller.index);
 
+// Detalle de producto 
 router.get("/detail/:id", controller.detail);
 
+// Carrito de compras
 router.get("/cart", controller.cart);
 
+// Formulario de creación de producto
 router.get("/create", controller.create);
 
+// Procesamiento del formulario de creación 
+router.post("/create", controller.store);
+
+// Formulario de edición
 router.get("/edit/:id", controller.edit);
 
 module.exports = router;
