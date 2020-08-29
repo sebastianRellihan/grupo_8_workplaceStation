b7e270962583faf82eb200ffdc697aaf7e6cc412
sebastianRellihan <57239382+sebastianRellihan@users.noreply.github.com>
2020-08-29 00:41:31 -0300

    Configuración de Multer como Factory Method

---
+++ site/src/config/multerConfig.js
@@ -0,0 +1,41 @@
+/**
+ * Función que utiliza el patrón factory method para crear y retornar un objeto de configuración para multer. 
+ * Recibe como parámetro el nombre de la carpeta, que debe encontrarse dentro de public/img, en donde se guardarán las imágenes.
+ * Ésta debe ser pasada como parámetro para crear el middleware de
+ * subida de archivos en el enrutador de productos: upload = multer(obj).
+*/
+
+const multer = require("multer");
+const path = require("path");
+
+// Tipos de archivos soportados
+const ALLOWED_MIME_TYPES = ["image/jpg", "image/jpeg", "image/png", "image/gif"];
+
+module.exports = function(destinationFolder) {
+    return {
+        /*
+        * Motor de subida de archivos. Define el directorio en el que se almacenan los
+        * archivos y su nombre.
+        */
+        storage : multer.diskStorage({
+            destination: function (req, file, cb) {
+                cb(null, path.join(__dirname, "..", "..", "public", "img", destinationFolder));
+            },
+            filename: function (req, file, cb) {
+                //     nombre del archivo  - fecha de subida  .extensión original
+                cb(null, file.fieldname + file.originalname.slice(0,3) + "-" + Date.now() + path.extname(file.originalname));
+            }
+        }),
+        /*
+        * Define el filtrado de los archivos. No arroja errores al insertarse archivos
+        * que no pasen el filtro, simplemente omite su subida.
+        */
+        fileFilter : function(req, file, cb){
+            if(ALLOWED_MIME_TYPES.includes(file.mimetype)){
+                cb(null, true); // Permite la subida
+            } else {
+                cb(null, false) // Omite la subida
+            }
+        }
+    }
+};
