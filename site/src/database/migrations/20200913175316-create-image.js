a5c0678e8a3f48cc186953c91ccbe2fd64196971
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-09-13 14:54:34 -0300

    Autogenerados el modelo y migración de la entidad 'image'

---
+++ site/src/database/migrations/20200913175316-create-image.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('images');
+  }
+};
