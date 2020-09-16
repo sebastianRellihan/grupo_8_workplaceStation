3ed341113983db2ac637a04f9e825b5662af330a
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-09-16 11:16:56 -0300

    Cambiado el nombre del modelo de 'user' a 'users' en las relaciones para que se corresponda con el nombre del modelo

--- site/src/database/models/purchase.js
+++ site/src/database/models/purchase.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            this.belongsTo(models.users);
+            this.belongsTo(models.user);
             this.belongsTo(models.shipping);
             this.belongsToMany(models.product, { through: 'products_purchases' });
         }
--- site/src/database/models/token.js
+++ site/src/database/models/token.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            this.belongsTo(models.users);
+            this.belongsTo(models.user);
         }
     };
     token.init({
