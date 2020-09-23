8a5f03454f3983c7ff26930cb4a002a126c9a0dd
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-09-23 16:42:20 -0300

    Contemplados los métodos de envío en la vista y el controlador de compras

--- site/src/controller/purchaseController.js
+++ site/src/controller/purchaseController.js
@@ -10,14 +10,23 @@ module.exports = {
         if(req.session.cart && req.session.cart.length != 0){
             // Obtengo promesas para todos los productos lamacenados en el carrito de la sesión
             req.session.cart.forEach(index => {
-                promises.push(product.findByPk(index, {
-                    include : image
-                }));
+                promises.push(product.findByPk(index));
             });
+
+            // Promesa para los métodos de envío
+            promises.push(shipping.findAll());
+
             // Ejecución de promesas
             Promise.all(promises)
-                .then(products => {
-                    res.render("products/purchase", { products });
+                .then(results => {
+                    // Obtengo el resultado de la última promesa (categorías),
+                    // e método .pop() tambien elimina el último resultado del array
+                    let shippings = results.pop();
+
+                    res.render("products/purchase", { 
+                        products : results,
+                        shippings : shippings
+                    });
                 })
                 .catch(error => {
                     // Error 500 "Internal server error"
