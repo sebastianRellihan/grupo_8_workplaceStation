184d4cc81e630991b349ea2a54ebe5d4817aee95
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-10-12 17:07:59 -0300

    Englobado el código dentro del evento 'window.onLoad' para evitar errores de carga

--- site/public/js/validators/authValidator.js
+++ site/public/js/validators/authValidator.js
@@ -1,69 +1,72 @@
-let errors = {}; // Contenedor global de errores
+window.addEventListener("load", function(){
 
-// ************* Elementos *************
+    let errors = {}; // Contenedor global de errores
 
-let userInput = document.getElementById("user-input");
-let password = document.getElementById("password");
-let userInputAlert = document.getElementById("user-input-alert");
-let passwordAlert = document.getElementById("password-alert");
+    // ************* Elementos *************
 
-let form = document.getElementById("login-form");
+    let userInput = document.getElementById("user-input");
+    let password = document.getElementById("password");
+    let userInputAlert = document.getElementById("user-input-alert");
+    let passwordAlert = document.getElementById("password-alert");
 
-// ************* Validaciones *************
+    let form = document.getElementById("login-form");
 
-function validateUserInput(){
-    // Se evitan los espacios por delante y por detras
-    userInput.value = userInput.value.trim();
-    
-    let feedback;
+    // ************* Validaciones *************
 
-    if(userInput.value == "") {
-        feedback = "Campo obligatório";
-    } else if(userInput.value.length < 2 || name.value.length > 255 ) feedback = "Debe tener entre 2 y 255 caracteres";
+    function validateUserInput(){
+        // Se evitan los espacios por delante y por detras
+        userInput.value = userInput.value.trim();
+        
+        let feedback;
 
-    if (feedback) {
-        userInput.classList.add("input-alert");
-        userInputAlert.innerText = feedback;
-    } else {
-        userInput.classList.remove("input-alert");
-        userInputAlert.innerText = "";
+        if(userInput.value == "") {
+            feedback = "Campo obligatório";
+        } else if(userInput.value.length < 2 || name.value.length > 255 ) feedback = "Debe tener entre 2 y 255 caracteres";
+
+        if (feedback) {
+            userInput.classList.add("input-alert");
+            userInputAlert.innerText = feedback;
+        } else {
+            userInput.classList.remove("input-alert");
+            userInputAlert.innerText = "";
+        }
     }
-}
 
-function validatePassword(){
-    
-    let feedback;
+    function validatePassword(){
+        
+        let feedback;
 
-    if(password.value == "") {
-        feedback = "Campo obligatório";
-    } else if(password.value.length > 255) {
-        feedback = "La contraseña no puede tener más de 255 caracteres";
-    } else if(password.value.length < 8) {
-        feedback = "Debe tener al menos 8 caracteres";
-    } 
+        if(password.value == "") {
+            feedback = "Campo obligatório";
+        } else if(password.value.length > 255) {
+            feedback = "La contraseña no puede tener más de 255 caracteres";
+        } else if(password.value.length < 8) {
+            feedback = "Debe tener al menos 8 caracteres";
+        } 
 
-    if (feedback) {
-        password.classList.add("input-alert");
-        passwordAlert.innerText = feedback;
-    } else {
-        password.classList.remove("input-alert");
-        passwordAlert.innerText = "";
+        if (feedback) {
+            password.classList.add("input-alert");
+            passwordAlert.innerText = feedback;
+        } else {
+            password.classList.remove("input-alert");
+            passwordAlert.innerText = "";
+        }
     }
-}
 
-// ************* Controladores de eventos *************
+    // ************* Controladores de eventos *************
 
-userInput.addEventListener("blur", validateUserInput);
-password.addEventListener("blur", validatePassword);
+    userInput.addEventListener("blur", validateUserInput);
+    password.addEventListener("blur", validatePassword);
 
-form.addEventListener("submit", function(event){
-    // Ataja el caso en que se intente enviar el formulario sin haber interactuado con los campos
-    validateUserInput();
-    validatePassword();
+    form.addEventListener("submit", function(event){
+        // Ataja el caso en que se intente enviar el formulario sin haber interactuado con los campos
+        validateUserInput();
+        validatePassword();
 
-    if(Object.keys(errors).length){
-        event.preventDefault();
-    }
+        if(Object.keys(errors).length){
+            event.preventDefault();
+        }
+
+    });
 
 });
-
