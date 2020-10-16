d3caf5dcbc35ddb69bafae49c69ab75cb71eb674
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-10-16 15:43:11 -0300

    Implementado y testeado el endpoint para obtener todos los usuarios

--- site/src/controller/api/usersController.js
+++ site/src/controller/api/usersController.js
@@ -2,9 +2,44 @@
  * Controlador que provee servicios al enrutador de usuarios encargándose de cerrar el ciclo 
  * solicitud/respuesta enviando datos en formato JSON.
  */
+const { user } = require("../../database/models");
 
 module.exports = {
     all : (req, res) => {
+
+        user.findAll()
+            .then(results => {
+
+                let response = {
+                    meta : {
+                        status : 200,
+                        statusMsg : "Ok",
+                        count : results.length
+                    },
+                    data : []
+                }
+
+                results.forEach(element => {
+                    response.data.push({
+                        id : element.id,
+                        name : element.name,
+                        email : element.email,
+                        detail : `/api/users/${element.id}`
+                    });
+                });
+
+                res.status(200).json(response);
+
+            })
+            .catch(error => {
+                res.status(500).json({
+                    meta : {
+                        status : 500,
+                        statusMsg : "Internal server error"
+                    },
+                    data : []
+                });
+            });
 
     },
 
