363f082dfbf1410852c5df37a254bb66e5f0d173
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-10-18 15:34:26 -0300

    Implementado el método para obtener todas las compras

--- site/src/controller/api/purchaseController.js
+++ site/src/controller/api/purchaseController.js
@@ -5,8 +5,61 @@
 const { purchase, product, shipping } = require("../../database/models");
 
 module.exports = {
-
+    /** Retorna información de todas las compras y los products asociados */
     all : (req, res) => {
+        
+    purchase.findAll({ include : 
+        [ product, shipping ] 
+    })
+        .then(results => {
+
+            let response = {
+                meta : {
+                    status : 200,
+                    statusMsg : "Ok",
+                    count : results.length,
+                    total : 0,
+                },
+                data : []
+            }
+
+            // Datos de cada compra
+            results.forEach(element => {
+                response.meta.total += Number.parseFloat(element.totalValue);
+
+                // Products asociados a la compra
+                let associatedProducts = element.products.map(prod => {
+                    return {
+                        id : prod.id,
+                        name : prod.name,
+                        price : prod.price,
+                        url : `/api/products/${prod.id}`
+                    }
+                })
+
+                response.data.push({
+                    id : element.id,
+                    purchasedAt : element.purchasedAt,
+                    shippingAddress : element.shippingAddress,
+                    shippingMethod : element.shipping.name,
+                    comment : element.comment,
+                    url : `/api/purchase/${element.id}`,
+                    products : associatedProducts
+                });
+            });
+
+            return res.json(response);
+
+        })
+        .catch(error => {
+            return res.status(500).json({
+                meta : {
+                    status : 500,
+                    statusMsg : "Internal server error"
+                },
+                data : []
+            });
+        });
 
     },
 
