82c3f765591dcc675d6fb70bf3738c41beaea442
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-10-22 23:22:16 -0300

    Integrada la primera build del dashbard desarrollado paralelamete en React

---
+++ site/public/build/precache-manifest.148991928ccd3873cb4a92fe24c142da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e6e32e7417989fe312321c78dfec67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18b3dffb9bf07ce7df34",
+    "url": "/static/js/2.42d70e16.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.42d70e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2e05b8e13cade5ebb5",
+    "url": "/static/js/main.fc82449f.chunk.js"
+  },
+  {
+    "revision": "00efbaf894789c612f6a",
+    "url": "/static/js/runtime-main.3edd2475.js"
+  }
+]);
