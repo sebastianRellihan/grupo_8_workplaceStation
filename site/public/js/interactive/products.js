fff916cf9355c001837d1722d1da423cdd12e688
sebastianRellihan <57239382+sebastianRellihan@users.noreply.github.com>
2020-10-23 10:17:23 -0300

    Botones de filtrado de productos mobile interactivos

---
+++ site/public/js/interactive/products.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", () => {
+    if (document.body.offsetWidth < 1024) {
+        let filterButton = document.getElementById("filter-button");
+        let categoryButton = document.getElementById("category-button");
+        let orderButton = document.getElementById("order-button");
+        let orderOptions = document.getElementById("order-products");
+        let categoriesOptions = document.getElementById("categories-products");
+        let filterOptions = document.getElementById("filter-products");
+    
+        filterButton.addEventListener("click", () => {
+            if (filterOptions.style.display == "none") {
+                filterOptions.style.display = "block";
+            } else {
+                filterOptions.style.display = "none";
+            }
+        })
+    
+        categoryButton.addEventListener("click", () => {
+            if (categoriesOptions.style.display == "none") {
+                categoriesOptions.style.display = "block";
+            } else {
+                categoriesOptions.style.display = "none";
+            }
+        })
+    
+        orderButton.addEventListener("click", () => {
+            if (orderOptions.style.display == "none") {
+                orderOptions.style.display = "block";
+            } else {
+                orderOptions.style.display = "none";
+            }
+        })
+    }
+})
