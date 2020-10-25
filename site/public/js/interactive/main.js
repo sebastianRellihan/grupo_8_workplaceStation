dbc67c2d4c461a3fc955307ad3f00dd124b84357
sebastianRellihan <57239382+sebastianRellihan@users.noreply.github.com>
2020-10-25 15:28:51 -0300

    Carrusel de bootstrap remplazado por un slider vanilla js en la home page

---
+++ site/public/js/interactive/main.js
@@ -0,0 +1,45 @@
+window.addEventListener("load", () => {
+    let imgSlider = document.getElementById("img-slider");
+    let imgSliderTitle = document.getElementById("img-slider-title");
+    let imgSliderCaption = document.getElementById("img-slider-caption");
+    let slides = [
+        {
+            img: "../img/slider-1.jpg",
+            title: "Workplace Station",
+            caption: "Marca líder en ergonomía y armado de estaciones de trabajo"
+        },
+        {
+            img: "../img/slider-3.jpg",
+            title: "Armá tu estación de trabajo",
+            caption: "Te ofrecemos una amplia cantidad de productos para un armado flexible y personalizado"
+        },
+        {
+            img: "../img/carousel-3.png",
+            title: "Modelos prearmados",
+            caption: "¿No sabés por dónde empezar? Te ofrecemos modelos prearmados y completamente personalizables"
+        },
+        {
+            img: "../img/carousel-5.jpg",
+            title: "Transformá tu hogar en tu oficina",
+            caption: "En Workplace Stations conocemos las necesidades del ambiente de trabajo moderno, podés encontrar todo lo que necesites y te lo llevamos a tu casa"
+        },
+        {
+            img: "../img/carousel-2.jpg",
+            title: "Amplia selección de asientos",
+            caption: "Sabemos que tu postura y salud son importantes, por eso te ofrecemos una amplia selección de asientos con altos estándares de calidad"
+        }
+    ];
+
+    function slide() {
+        slides.forEach((slide, i) => { 
+            setTimeout(() => {
+                imgSlider.style.backgroundImage = `url(${slide.img})`;
+                imgSlider.style.transition = "2s";
+                imgSliderTitle.innerText = slide.title;
+                imgSliderCaption.innerText = slide.caption;
+            }, i * 6000);
+        });
+        setTimeout(slide, 5000 * slides.length)
+    }
+    slide();
+})
